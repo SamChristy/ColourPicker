d8f3bce751aba15784f105153210143627553012
Sam Christy <sam_christy@hotmail.co.uk>
2021-01-29 18:54:40 +0000

    Add component index.js, to make import cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ColourPicker from "./components/ColourPicker/ColourPicker";
+import { ColourPicker } from "./components";
 import './App.css';
 
 export default function App() {
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as ColourPicker } from './ColourPicker/ColourPicker'
