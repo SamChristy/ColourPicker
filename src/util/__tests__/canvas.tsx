2d95553587ba0690c1604778eb98b58930ac2333
Sam Christy <sam_christy@hotmail.co.uk>
2021-03-23 22:01:16 +0000

    Test getDimensions()

--- src/util/__tests__/canvas.tsx
+++ src/util/__tests__/canvas.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
-import { getClickCoords } from '../canvas';
+import { getClickCoords, getDimensions } from '../canvas';
 
 describe('getClickCoords()', () => {
   it('returns the coordinates of the click on a DOM element', () => {
@@ -16,5 +16,33 @@ describe('getClickCoords()', () => {
     const click = new MouseEvent('click', { clientX: 30, clientY: 85 });
 
     expect(getClickCoords(div, click)).toEqual({ x: 20, y: 70 });
+  });
+});
+
+describe('getDimensions()', () => {
+  const testGetDimensions = () => {
+    const rect = {
+      ...global.emptyDOMRect,
+      width: 50,
+      height: 70,
+    };
+    jest.spyOn(HTMLElement.prototype, 'getBoundingClientRect').mockImplementation(() => rect);
+    render(<canvas data-testid={1} />);
+    const canvas = screen.getByTestId(1);
+    // @ts-ignore (Because getByTestId always returns a HTMLElement.)
+    const output = getDimensions(canvas);
+
+    return { rect, canvas, output };
+  };
+  it("returns the canvas's dimensions", () => {
+    const { rect, output } = testGetDimensions();
+
+    expect(output).toEqual(rect);
+  });
+  it("sets the canvas's width & height attributes", () => {
+    const { rect, canvas } = testGetDimensions();
+
+    expect(canvas).toHaveAttribute('width', String(rect.width));
+    expect(canvas).toHaveAttribute('height', String(rect.height));
   });
 });
