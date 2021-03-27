3f132d5df5c51eb44c106519ad69fabb2f098c62
Sam Christy <sam_christy@hotmail.co.uk>
2021-03-27 22:35:48 +0000

    Update marker tests, to cover new click proxy functionality

--- src/components/ColourPicker/__tests__/Marker.tsx
+++ src/components/ColourPicker/__tests__/Marker.tsx
@@ -1,6 +1,18 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Marker from '../Marker';
 
 it('renders without crashing', () => {
-  render(<Marker position={{ x: 0, y: 0 }} />);
+  render(<Marker position={{ x: 0, y: 0 }} onClick={() => {}} />);
+});
+
+it('proxies click events', () => {
+  const mockCallback = jest.fn(() => {});
+  const { container } = render(<Marker position={{ x: 0, y: 0 }} onClick={mockCallback} />);
+  const marker = container.children[0];
+  const click = { clientX: 2, clientY: 5 };
+
+  userEvent.click(marker, click);
+
+  expect(mockCallback).toHaveBeenCalledWith(expect.objectContaining(click));
 });
